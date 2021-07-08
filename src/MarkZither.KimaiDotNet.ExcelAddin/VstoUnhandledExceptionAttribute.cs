832b26944bb651dd5e8f9c7b6708889a8134436e
MarkZither <mark.burton@zither-it.co.uk>
2021-07-08 23:05:26 +0200

    tidy usings in VstoUnhandledExceptionAttribute

--- src/MarkZither.KimaiDotNet.ExcelAddin/VstoUnhandledExceptionAttribute.cs
+++ src/MarkZither.KimaiDotNet.ExcelAddin/VstoUnhandledExceptionAttribute.cs
@@ -3,12 +3,6 @@
 using PostSharp.Aspects;
 using PostSharp.Serialization;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MarkZither.KimaiDotNet.ExcelAddin
 {
     [PSerializable]
