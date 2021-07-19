f751bfbb2dea3133fb5ef1348a4e01e4a56f8da4
MarkZither <mark.burton@zither-it.co.uk>
2021-07-19 22:42:25 +0200

    Add stryker to try out mutation testing

--- src/MarkZither.KimaiDotNet.ExcelAddin/KimaiRibbon.Designer.cs
+++ src/MarkZither.KimaiDotNet.ExcelAddin/KimaiRibbon.Designer.cs
@@ -37,35 +37,35 @@ namespace MarkZither.KimaiDotNet.ExcelAddin
         {
             this.tab1 = this.Factory.CreateRibbonTab();
             this.grpSettings = this.Factory.CreateRibbonGroup();
+            this.tglApiCreds = this.Factory.CreateRibbonToggleButton();
+            this.btnConnect = this.Factory.CreateRibbonButton();
             this.grpTimesheets = this.Factory.CreateRibbonGroup();
+            this.btnSync = this.Factory.CreateRibbonButton();
             this.grpCalendar = this.Factory.CreateRibbonGroup();
+            this.btnCalendar = this.Factory.CreateRibbonButton();
+            this.btnSettings = this.Factory.CreateRibbonButton();
             this.box1 = this.Factory.CreateRibbonBox();
             this.lblStartDateTitle = this.Factory.CreateRibbonLabel();
             this.lblStartDate = this.Factory.CreateRibbonLabel();
             this.box2 = this.Factory.CreateRibbonBox();
             this.lblEndDateTitle = this.Factory.CreateRibbonLabel();
             this.lblEndDate = this.Factory.CreateRibbonLabel();
+            this.btnSyncCalendarCategories = this.Factory.CreateRibbonButton();
             this.grpAdmin = this.Factory.CreateRibbonGroup();
+            this.btnSyncCustomers = this.Factory.CreateRibbonButton();
+            this.btnSyncProjects = this.Factory.CreateRibbonButton();
             this.grpSupport = this.Factory.CreateRibbonGroup();
+            this.btnBuy = this.Factory.CreateRibbonButton();
+            this.btnSponsor = this.Factory.CreateRibbonButton();
+            this.btnInfo = this.Factory.CreateRibbonButton();
+            this.btnBug = this.Factory.CreateRibbonButton();
+            this.btnHelp = this.Factory.CreateRibbonButton();
             this.grpVersion = this.Factory.CreateRibbonGroup();
             this.lblVersion = this.Factory.CreateRibbonLabel();
             this.lblAddinVersionNo = this.Factory.CreateRibbonLabel();
             this.separator1 = this.Factory.CreateRibbonSeparator();
             this.lblServerVersion = this.Factory.CreateRibbonLabel();
             this.lblVersionNo = this.Factory.CreateRibbonLabel();
-            this.tglApiCreds = this.Factory.CreateRibbonToggleButton();
-            this.btnConnect = this.Factory.CreateRibbonButton();
-            this.btnSync = this.Factory.CreateRibbonButton();
-            this.btnCalendar = this.Factory.CreateRibbonButton();
-            this.btnSettings = this.Factory.CreateRibbonButton();
-            this.btnSyncCalendarCategories = this.Factory.CreateRibbonButton();
-            this.btnSyncCustomers = this.Factory.CreateRibbonButton();
-            this.btnSyncProjects = this.Factory.CreateRibbonButton();
-            this.btnBuy = this.Factory.CreateRibbonButton();
-            this.btnSponsor = this.Factory.CreateRibbonButton();
-            this.btnInfo = this.Factory.CreateRibbonButton();
-            this.btnBug = this.Factory.CreateRibbonButton();
-            this.btnHelp = this.Factory.CreateRibbonButton();
             this.tab1.SuspendLayout();
             this.grpSettings.SuspendLayout();
             this.grpTimesheets.SuspendLayout();
@@ -96,11 +96,40 @@ namespace MarkZither.KimaiDotNet.ExcelAddin
             this.grpSettings.Label = "Settings";
             this.grpSettings.Name = "grpSettings";
             // 
+            // tglApiCreds
+            // 
+            this.tglApiCreds.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.tglApiCreds.Label = "API Credentials";
+            this.tglApiCreds.Name = "tglApiCreds";
+            this.tglApiCreds.OfficeImageId = "PivotTableFieldSettings";
+            this.tglApiCreds.ScreenTip = "test tip";
+            this.tglApiCreds.ShowImage = true;
+            this.tglApiCreds.SuperTip = "test super tip";
+            this.tglApiCreds.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.tglApiCreds_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Label = "Connect";
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.OfficeImageId = "ConnectToServer";
+            this.btnConnect.ShowImage = true;
+            this.btnConnect.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnConnect_Click);
+            // 
             // grpTimesheets
             // 
             this.grpTimesheets.Items.Add(this.btnSync);
             this.grpTimesheets.Label = "Timesheets";
             this.grpTimesheets.Name = "grpTimesheets";
+            // 
+            // btnSync
+            // 
+            this.btnSync.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnSync.Enabled = false;
+            this.btnSync.Label = "Sync Data";
+            this.btnSync.Name = "btnSync";
+            this.btnSync.OfficeImageId = "ReplicationSynchronizeNow";
+            this.btnSync.ShowImage = true;
+            this.btnSync.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSync_Click);
             // 
             // grpCalendar
             // 
@@ -111,6 +140,24 @@ namespace MarkZither.KimaiDotNet.ExcelAddin
             this.grpCalendar.Items.Add(this.btnSyncCalendarCategories);
             this.grpCalendar.Label = "Calendar Import";
             this.grpCalendar.Name = "grpCalendar";
+            // 
+            // btnCalendar
+            // 
+            this.btnCalendar.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnCalendar.Label = "Import Calendar";
+            this.btnCalendar.Name = "btnCalendar";
+            this.btnCalendar.OfficeImageId = "CalendarImport";
+            this.btnCalendar.ShowImage = true;
+            this.btnCalendar.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnCalendar_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnSettings.Label = "Settings";
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.OfficeImageId = "PivotTableFieldSettings";
+            this.btnSettings.ShowImage = true;
+            this.btnSettings.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSettings_Click);
             // 
             // box1
             // 
@@ -144,104 +191,6 @@ namespace MarkZither.KimaiDotNet.ExcelAddin
             this.lblEndDate.Label = "...";
             this.lblEndDate.Name = "lblEndDate";
             // 
-            // grpAdmin
-            // 
-            this.grpAdmin.Items.Add(this.btnSyncCustomers);
-            this.grpAdmin.Items.Add(this.btnSyncProjects);
-            this.grpAdmin.Label = "Admin";
-            this.grpAdmin.Name = "grpAdmin";
-            // 
-            // grpSupport
-            // 
-            this.grpSupport.Items.Add(this.btnBuy);
-            this.grpSupport.Items.Add(this.btnSponsor);
-            this.grpSupport.Items.Add(this.btnInfo);
-            this.grpSupport.Items.Add(this.btnBug);
-            this.grpSupport.Items.Add(this.btnHelp);
-            this.grpSupport.Label = "Support";
-            this.grpSupport.Name = "grpSupport";
-            // 
-            // grpVersion
-            // 
-            this.grpVersion.Items.Add(this.lblVersion);
-            this.grpVersion.Items.Add(this.lblAddinVersionNo);
-            this.grpVersion.Items.Add(this.separator1);
-            this.grpVersion.Items.Add(this.lblServerVersion);
-            this.grpVersion.Items.Add(this.lblVersionNo);
-            this.grpVersion.Label = "Version";
-            this.grpVersion.Name = "grpVersion";
-            // 
-            // lblVersion
-            // 
-            this.lblVersion.Label = "Addin Version";
-            this.lblVersion.Name = "lblVersion";
-            // 
-            // lblAddinVersionNo
-            // 
-            this.lblAddinVersionNo.Label = "0.0.0.0";
-            this.lblAddinVersionNo.Name = "lblAddinVersionNo";
-            // 
-            // separator1
-            // 
-            this.separator1.Name = "separator1";
-            // 
-            // lblServerVersion
-            // 
-            this.lblServerVersion.Label = "Server Version";
-            this.lblServerVersion.Name = "lblServerVersion";
-            // 
-            // lblVersionNo
-            // 
-            this.lblVersionNo.Label = "Not Connected";
-            this.lblVersionNo.Name = "lblVersionNo";
-            // 
-            // tglApiCreds
-            // 
-            this.tglApiCreds.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
-            this.tglApiCreds.Label = "API Credentials";
-            this.tglApiCreds.Name = "tglApiCreds";
-            this.tglApiCreds.OfficeImageId = "PivotTableFieldSettings";
-            this.tglApiCreds.ScreenTip = "test tip";
-            this.tglApiCreds.ShowImage = true;
-            this.tglApiCreds.SuperTip = "test super tip";
-            this.tglApiCreds.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.tglApiCreds_Click);
-            // 
-            // btnConnect
-            // 
-            this.btnConnect.Label = "Connect";
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.OfficeImageId = "ConnectToServer";
-            this.btnConnect.ShowImage = true;
-            this.btnConnect.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnConnect_Click);
-            // 
-            // btnSync
-            // 
-            this.btnSync.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
-            this.btnSync.Enabled = false;
-            this.btnSync.Label = "Sync Data";
-            this.btnSync.Name = "btnSync";
-            this.btnSync.OfficeImageId = "ReplicationSynchronizeNow";
-            this.btnSync.ShowImage = true;
-            this.btnSync.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSync_Click);
-            // 
-            // btnCalendar
-            // 
-            this.btnCalendar.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
-            this.btnCalendar.Label = "Import Calendar";
-            this.btnCalendar.Name = "btnCalendar";
-            this.btnCalendar.OfficeImageId = "CalendarImport";
-            this.btnCalendar.ShowImage = true;
-            this.btnCalendar.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnCalendar_Click);
-            // 
-            // btnSettings
-            // 
-            this.btnSettings.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
-            this.btnSettings.Label = "Settings";
-            this.btnSettings.Name = "btnSettings";
-            this.btnSettings.OfficeImageId = "PivotTableFieldSettings";
-            this.btnSettings.ShowImage = true;
-            this.btnSettings.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSettings_Click);
-            // 
             // btnSyncCalendarCategories
             // 
             this.btnSyncCalendarCategories.Label = "Import Categories";
@@ -249,6 +198,13 @@ namespace MarkZither.KimaiDotNet.ExcelAddin
             this.btnSyncCalendarCategories.OfficeImageId = "DataRefreshDialog";
             this.btnSyncCalendarCategories.ShowImage = true;
             this.btnSyncCalendarCategories.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSyncCalendarCategories_Click);
+            // 
+            // grpAdmin
+            // 
+            this.grpAdmin.Items.Add(this.btnSyncCustomers);
+            this.grpAdmin.Items.Add(this.btnSyncProjects);
+            this.grpAdmin.Label = "Admin";
+            this.grpAdmin.Name = "grpAdmin";
             // 
             // btnSyncCustomers
             // 
@@ -261,6 +217,16 @@ namespace MarkZither.KimaiDotNet.ExcelAddin
             this.btnSyncProjects.Label = "Sync Projects";
             this.btnSyncProjects.Name = "btnSyncProjects";
             this.btnSyncProjects.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnSyncPremuim_Click);
+            // 
+            // grpSupport
+            // 
+            this.grpSupport.Items.Add(this.btnBuy);
+            this.grpSupport.Items.Add(this.btnSponsor);
+            this.grpSupport.Items.Add(this.btnInfo);
+            this.grpSupport.Items.Add(this.btnBug);
+            this.grpSupport.Items.Add(this.btnHelp);
+            this.grpSupport.Label = "Support";
+            this.grpSupport.Name = "grpSupport";
             // 
             // btnBuy
             // 
@@ -304,6 +270,40 @@ namespace MarkZither.KimaiDotNet.ExcelAddin
             this.btnHelp.OfficeImageId = "AccessOnlineLists";
             this.btnHelp.ShowImage = true;
             this.btnHelp.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnHelp_Click);
+            // 
+            // grpVersion
+            // 
+            this.grpVersion.Items.Add(this.lblVersion);
+            this.grpVersion.Items.Add(this.lblAddinVersionNo);
+            this.grpVersion.Items.Add(this.separator1);
+            this.grpVersion.Items.Add(this.lblServerVersion);
+            this.grpVersion.Items.Add(this.lblVersionNo);
+            this.grpVersion.Label = "Version";
+            this.grpVersion.Name = "grpVersion";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Label = "Addin Version";
+            this.lblVersion.Name = "lblVersion";
+            // 
+            // lblAddinVersionNo
+            // 
+            this.lblAddinVersionNo.Label = "0.0.0.0";
+            this.lblAddinVersionNo.Name = "lblAddinVersionNo";
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // lblServerVersion
+            // 
+            this.lblServerVersion.Label = "Server Version";
+            this.lblServerVersion.Name = "lblServerVersion";
+            // 
+            // lblVersionNo
+            // 
+            this.lblVersionNo.Label = "Not Connected";
+            this.lblVersionNo.Name = "lblVersionNo";
             // 
             // KimaiRibbon
             // 
---
+++ tests/KimaiDotNet.Core.Tests/DebugTracerTests.cs
@@ -0,0 +1,150 @@
+﻿using FakeItEasy;
+
+using MarkZither.KimaiDotNet.Core;
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+using Xunit;
+
+namespace MarkZither.KimaiDotNet.Core.Tests
+{
+    public class DebugTracerTests
+    {
+
+
+        public DebugTracerTests()
+        {
+
+        }
+
+        private DebugTracer CreateDebugTracer()
+        {
+            return new DebugTracer();
+        }
+
+        [Fact]
+        public void Information_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var debugTracer = this.CreateDebugTracer();
+            string message = null;
+
+            // Act
+            debugTracer.Information(
+                message);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void TraceError_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var debugTracer = this.CreateDebugTracer();
+            string invocationId = null;
+            Exception exception = null;
+
+            // Act
+            debugTracer.TraceError(
+                invocationId,
+                exception);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ReceiveResponse_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var debugTracer = this.CreateDebugTracer();
+            string invocationId = null;
+            HttpResponseMessage response = null;
+
+            // Act
+            debugTracer.ReceiveResponse(
+                invocationId,
+                response);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void SendRequest_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var debugTracer = this.CreateDebugTracer();
+            string invocationId = null;
+            HttpRequestMessage request = null;
+
+            // Act
+            debugTracer.SendRequest(
+                invocationId,
+                request);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Configuration_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var debugTracer = this.CreateDebugTracer();
+            string source = null;
+            string name = null;
+            string value = null;
+
+            // Act
+            debugTracer.Configuration(
+                source,
+                name,
+                value);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void EnterMethod_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var debugTracer = this.CreateDebugTracer();
+            string invocationId = null;
+            object instance = null;
+            string method = null;
+            IDictionary<string, object> parameters = null;
+
+            // Act
+            debugTracer.EnterMethod(
+                invocationId,
+                instance,
+                method,
+                parameters);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ExitMethod_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var debugTracer = this.CreateDebugTracer();
+            string invocationId = null;
+            object returnValue = null;
+
+            // Act
+            debugTracer.ExitMethod(
+                invocationId,
+                returnValue);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
